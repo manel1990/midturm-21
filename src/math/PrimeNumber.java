f67c94bd20a923889ed599e9ec496ef0efa3a286
manel ketem <manelketem1990@gmail.com>
2021-10-17 17:46:59 -0400

    finabonacci

--- src/math/PrimeNumber.java
+++ src/math/PrimeNumber.java
@@ -14,8 +14,27 @@ public class PrimeNumber {
 		 */
 
 		//implementation here...
+		int i= 0;
+		int num = 0;
+		String primeNumber = "";
+		for (i = 1; i < 100; i++) {
+			int counter = 0;
+			for (num = i; num >= 1; num--) {
+				if (i % num == 0) {
+					counter = counter+1;
+
+				}
+			}
+			if (counter == 2) {
+				primeNumber = primeNumber + i + "";
+			}
+
+		}
+		System.out.println("prime numbers are;");
+		System.out.println(primeNumber);
 
 
 	}
-
 }
+
+
